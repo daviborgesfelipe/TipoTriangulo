022de3ad6fd3c0d92f0eaad2efe2623ff3c66931
Davi Borges <davifelipe_300@hotmail.com>
2023-04-05 17:01:33 -0300

    Add project files.

---
+++ TipoTriangulo.ConsoleApp/Triangulo.cs
@@ -0,0 +1,37 @@
+﻿namespace TipoTriangulo.ConsoleApp
+{
+    internal class Triangulo
+    {
+        internal int ladoUm;
+        internal int ladoDois;
+        internal int ladoTres;
+        internal void DescobrirTipoTriangulo()
+        {
+            if (ladoUm <= 0 || ladoDois <= 0 || ladoTres <= 0)
+            {
+                Console.WriteLine("\nO triangulo possui um ou mais lado com o valor igual ou menor que zero (0)");
+                Console.ReadLine();
+            }
+            else if (ladoUm > ladoDois + ladoTres || ladoDois > ladoUm + ladoTres || ladoTres > ladoUm + ladoDois)
+            {
+                Console.WriteLine("\nO triangulo pussui um lado com o valor maior que a soma dos outros dois valores");
+                Console.ReadLine();
+            }
+            else if (ladoUm == ladoDois && ladoDois == ladoTres)
+            {
+                Console.WriteLine("\nO triangulo é Equilatero possui todos os lados com os mesmos valores");
+                Console.ReadLine();
+            }
+            else if (ladoUm == ladoDois || ladoDois == ladoTres || ladoTres == ladoUm)
+            {
+                Console.WriteLine("\nO triangulo é Isósceles possui dois lados com os valores iguais");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("\nO triangulo é Escaleno possui todos os lados de valores diferentes");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ TipoTriangulo.ConsoleApp/TrianguloTipo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipoTriangulo.ConsoleApp
+{
+    internal class TrianguloTipo
+    {
+        static void Main(string[] args)
+        {
+            Triangulo triangulo = new Triangulo();
+            while (true) 
+            {
+                Console.Clear();
+                Console.WriteLine("Digite o primeiro lado e aperte enter");
+                triangulo.ladoUm = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite o segundo lado e aperte enter");
+                triangulo.ladoDois = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite o terceiro lado e aperte enter");
+                triangulo.ladoTres = int.Parse(Console.ReadLine());
+                triangulo.DescobrirTipoTriangulo();
+            }
+        }
+    }
+}
